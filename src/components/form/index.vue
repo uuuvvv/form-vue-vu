993a536239972292a206afaa3d919781509e8b3f
uuuvvv <jyzhl11@163.com>
2022-05-17 15:17:50 +0800

    文件初始化

---
+++ src/components/form/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'formCom',
+  data () {
+    return {}
+  },
+  props: {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="sass" scoped></style>
+>
---
+++ src/views/formView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">123</div>
+</template>
+<script>
+export default {
+  name: 'formView',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
